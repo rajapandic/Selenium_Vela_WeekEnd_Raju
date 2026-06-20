c25367b611658924aba9616153706405877b0002
DELL <DELL@Rajapandi>
2026-06-20 15:44:38 +0530

    New Commit

---
+++ Com.DemoWebShop_Raju/src/main/java/Com/DemoWebShop_GenericUtility/ListenersUtility.java
@@ -0,0 +1,34 @@
+package Com.DemoWebShop_GenericUtility;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListenersUtility implements ITestListener
+{
+    WebDriver driver;
+    
+    @Override
+    public void onTestFailure(ITestResult result) 
+    {
+    	TakesScreenshot ts = (TakesScreenshot) BaseTest.sDriver;
+    	File temp = ts.getScreenshotAs(OutputType.FILE);
+    	File dest = new File(FrameWorkConstants.screenshotPath);
+    	try 
+    	{
+			FileHandler.copy(temp, dest);
+		} 
+    	catch (IOException e) 
+    	{
+			e.printStackTrace();
+		}
+    }
+}
+
+
